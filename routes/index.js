0db0d42ca5e1e39aa4fa7cefc3c8a6eb32504628
jgkahn33 <jgkahn33@gmail.com>
2017-10-04 17:23:25 -0400

    changed Pool() constructor

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Pool = require('pg-pool');
-var pool = new Pool({database:'guarded-anchorage-11814'});
+var pool = new Pool();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
