638e2ad2842434276a723c5e4138baf6500feef5
jgkahn33 <jgkahn33@gmail.com>
2017-10-13 12:31:12 -0400

    finished error pages for incorrect phone numbers

--- routes/index.js
+++ routes/index.js
@@ -8,49 +8,100 @@ var config = {
        "updatedAt": "updatedat"
      }
 };
-var sequelize = new Sequelize(process.env.DATABASE_URL, config);
-//var sequelize = new Sequelize('postgres://root:abacus@localhost:5432/clientsdb');
+//var sequelize = new Sequelize(process.env.DATABASE_URL, config);
+var sequelize = new Sequelize('postgres://root:abacus@localhost:5432/clientsdb');
 // var pool = new Pool();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.render('bad_phone_driver');
+  res.render('index');
  });
 
 router.post('/addDriver', function(req, res){
-   var driver = sequelize.define('drivers', {
-     name: {
-       type: Sequelize.STRING
-     },
-     phone: {
-       type: Sequelize.STRING
-     }
-   });
-   driver.sync().then(() => {
-     return driver.create({
-       name: req.body.name,
-       phone: req.body.phone
-     });
-   });
-   res.redirect('/thankyou');
+   if(isNaN(parseInt(req.body.phone)))
+   {
+      console.log("nan");
+      res.render('bad_phone_driver');
+   }
+   else{
+      if(req.body.phone.length > 11)
+      {
+         console.log("greater than 11");
+         res.render('bad_phone_driver');
+      }
+      if(req.body.phone.length === 11)
+      {
+         console.log("equals 11");
+         if(req.body.phone.charAt(0) !== '1')
+         {
+            console.log("does not equal 1");
+            res.render('bad_phone_driver');
+         }
+      }
+      if(req.body.length < 10)
+      {
+         console.log("less than 10");
+         res.render('bad_phone_driver')
+      }
+      else{
+         console.log("ok");
+         var driver = sequelize.define('drivers', {
+           name: {
+             type: Sequelize.STRING
+           },
+           phone: {
+             type: Sequelize.STRING
+           }
+         });
+         driver.sync().then(() => {
+           return driver.create({
+             name: req.body.name,
+             phone: req.body.phone
+           });
+         });
+         res.redirect('/thankyou');
+      }
+   }
 });
 router.post('/addUser', function(req, res){
-   var user = sequelize.define('users', {
-     name: {
-       type: Sequelize.STRING
-     },
-     phone: {
-       type: Sequelize.STRING
-     }
-   });
-   user.sync().then(() => {
-     return user.create({
-       name: req.body.name,
-       phone: req.body.phone
-     });
-   });
-   res.redirect('/thankyou');
+   if(isNaN(parseInt(req.body.phone)))
+   {
+      res.render('bad_phone_user');
+   }
+   else{
+      if(req.body.phone.length > 11)
+      {
+         res.render('bad_phone_user');
+      }
+      else if(req.body.phone.length === 11)
+      {
+         if(req.body.phone.charAt(0) !== '1')
+         {
+            res.render('bad_phone_user');
+         }
+      }
+      else if(req.body.length < 10)
+      {
+         res.render('bad_phone_user')
+      }
+      else{
+         var user = sequelize.define('users', {
+           name: {
+             type: Sequelize.STRING
+           },
+           phone: {
+             type: Sequelize.STRING
+           }
+         });
+         user.sync().then(() => {
+           return user.create({
+             name: req.body.name,
+             phone: req.body.phone
+           });
+         });
+         res.redirect('/thankyou');
+      }
+   }
 });
 router.get('/thankyou', function(req, res){
    res.render('thankyou');
